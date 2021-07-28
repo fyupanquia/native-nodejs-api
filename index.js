4b6013604ea4079a65d72bb13432f8b6696155fb
fyupanquia <fyupanquia@outlook.com>
2021-07-28 10:54:49 -0500

    unit testinf

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cli = require("./lib/cli");
 const app = {};
 
 // Init function
-app.init = function () {
+app.init = function (callback) {
   // Start the server
   server.init();
 
@@ -22,11 +22,14 @@ app.init = function () {
   // Start the CLI, but make sure it starts last
   setTimeout(function () {
     cli.init();
+    callback();
   }, 50);
 };
 
-// Self executing
-app.init();
+// Self invoking only if required directly
+if (require.main === module) {
+  app.init(function () {});
+}
 
 // Export the app
 module.exports = app;
